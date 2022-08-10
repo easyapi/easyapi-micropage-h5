78e126b103b59dcfd8771ac849afc5631fb49223
chenyunc123 <1537299563@qq.com>
2022-08-10 10:48:57 +0800

    build

---
+++ api/request.js
@@ -0,0 +1,31 @@
+const baseUrl = '';
+
+export function request(url, method, data, header) {
+	let requestMethod = method.toUpperCase() || "GET";
+	let requestData = data || {};
+	let requestHeader = header || {
+		'content-type': 'application/json'
+	};
+	var promise = new Promise(function(resolve, reject) {
+		let token = uni.getStorageSync('authenticationToken');
+		if (token) {
+			requestHeader['Authorization'] = 'Bearer ' + token
+		}
+		let pathUrl = baseUrl + url
+		uni.request({
+			url: pathUrl,
+			data: requestData,
+			method: requestMethod,
+			dataType: "json",
+			header: requestHeader,
+			success: function(res) {
+				resolve(res)
+			},
+			fail: function(error) {
+				console.log(error)
+				reject
+			}
+		})
+	});
+	return promise;
+};
